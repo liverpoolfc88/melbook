808196382061aae07261ac3917f59d1ede72df1d
liverpoolfc88 <52122299+liverpoolfc88@users.noreply.github.com>
2021-01-23 16:13:03 +0500

    dsa
    dsa

--- _protected/modules/admin/views/layouts/left.php
+++ _protected/modules/admin/views/layouts/left.php
@@ -76,7 +76,8 @@ $action = Yii::$app->controller->id;
     /*}*/
 </style>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="/themes/jquery/jquery.min.js"></script>
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>-->
 <script>
     $(function () {
         $('.white').click(function () {
--- _protected/views/site/error.php
+++ _protected/views/site/error.php
@@ -2,21 +2,37 @@
 /* @var $this yii\web\View */
 /* @var $name string */
 /* @var $message string */
+
 /* @var $exception Exception */
 
 use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+<!--<div class="site-error">-->
+<!--    <div style="background-image: url('/themes/404.png');" class="site-error">-->
+<!---->
+<!--    <h1>--><? //= Html::encode($this->title) ?><!--</h1>-->
+<!---->
+<!--    <div class="alert alert-danger">-->
+<!--        --><? //= nl2br(Html::encode($message)) ?>
+<!--    </div>-->
+<!---->
+<!--    <p> --><? //= Yii::t('app', 'The above error occurred while the Web server was processing your request.') ?><!-- </p>-->
+<!--    <p> --><? //= Yii::t('app', 'Please contact us if you think this is a server error. Thank you.') ?><!-- </p>-->
+<!---->
+<!--</div>-->
+<section>
+    <div class="container">
+        <div class="row">
+            <marquee><h3 style="color: #0b58a2">Saxifa topilmadi!</h3></marquee>
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <a href="https://www.melbook.uz">
+                    <img style="width: 100%" src="/themes/404.png">
+                </a>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
     </div>
-
-    <p> <?= Yii::t('app', 'The above error occurred while the Web server was processing your request.') ?> </p>
-    <p> <?= Yii::t('app', 'Please contact us if you think this is a server error. Thank you.') ?> </p>
-
-</div>
+</section>
